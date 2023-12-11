b60edcdb2f56deb13a98326c71057ea16a5817cd
OferRavid <ofer.rvd@gmail.com>
2023-12-12 00:59:18 +0200

    added docstrings in main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import sys
 
 
 def main():
+    '''
+    The main function: calls the required function to collect the 
+    neccesary data and prints the report.
+    '''
+
     book_path = "books/frankenstein.txt"
     print(f"--- Begin report of {book_path} ---\n")
     text = get_book_text(book_path)
@@ -16,10 +21,19 @@ def main():
 
 
 def get_word_count(text: str):
+    '''
+    This function calculates and returns the word count in given text.
+    '''
+
     words = text.split()
     return len(words)
 
 def get_char_count(text: str):
+    '''
+    This function counts appearances of letters in given text,
+    and returns a dictionary of letter(lowercase):count.
+    '''
+
     char_dict = dict((char, 0) for char in list(string.ascii_lowercase))
     lowercase_text = text.lower()
     for char in lowercase_text:
@@ -28,8 +42,13 @@ def get_char_count(text: str):
     return char_dict
 
 def get_book_text(path):
+    '''
+    This function reads text from file using given path to file,
+    and returns the text extracted.'''
+    
     with open(path) as f:
         return f.read()
+
 
 main()
 
